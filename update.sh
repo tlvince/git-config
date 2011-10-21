6bb188a95d8ae16724a7d7d6d08fea6d2a351ba2
Tom Vincent <tlvince@gmail.com>
2011-10-21 13:47:53 +0100

    Add a quick'n'dirty gitingore update script

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Make a global git ignore.
+# Copyright 2011 Tom Vincent <http://tlvince.com/contact/>
+
+ignores="Archives Eclipse OSX SVN Windows"
+url="https://raw.github.com/github/gitignore/master/Global"
+out="$(git config core.excludesfile)"
+out="${out/\~/$HOME}"
+
+[ -f "$out" ] && rm "$out"
+
+cat << EOF >> "$out"
+# Global gitignore file. {{{1
+# Copyright 2011 Tom Vincent <http://tlvince.com/contact/>
+# vim: fdm=marker
+
+EOF
+
+for i in $ignores; do
+    echo "# $i {{{1" >> "$out"
+    curl -sS "$url/$i.gitignore" >> "$out"
+    echo "" >> "$out"
+done
