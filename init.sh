6c1f5aa0fc79bb50239b0b37db78b0ac7a1785dc
Tom Vincent <git@tlvince.com>
2013-04-29 22:00:48 +0800

    CONFIG -> DATA: better suited

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 # © 2013 Tom Vincent <http://tlvince.com/contact>
 
 out="$HOME/.gitignore"
-vendor="$XDG_CONFIG_HOME/git/vendor/gitignore/Global"
+vendor="$XDG_DATA_HOME/git/vendor/gitignore/Global"
 
 cat $vendor/{Archives,SVN,Linux,Tags,OSX}.gitignore > "$out"
 
 [ -f "$HOME/.gitconfig" ] || \
-  ln -s "$XDG_CONFIG_HOME/git/git.conf" "$HOME/.gitconfig"
+  ln -s "$XDG_DATA_HOME/git/git.conf" "$HOME/.gitconfig"
