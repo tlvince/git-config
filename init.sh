c2af3746c6f32cebc1c1303e934cef31e9140967
Tom Vincent <git@tlvince.com>
2013-10-27 14:59:36 +0000

    Localise data files and gitignore generator

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Initialise Git ignore.
+# © 2013 Tom Vincent <http://tlvince.com/contact>
+
+vendor=".gitdata/vendor/gitignore/Global"
+cat $vendor/{Archives,SVN,Tags,OSX}.gitignore > ".gitdata/gitignore"
