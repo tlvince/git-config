db56db25b88a950bac0973aedeca7d4644f6b797
Tom Vincent <tom.vincent@clearscore.com>
2020-01-15 10:39:24 +0000

    fix(gitignore): osx -> macos

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 # © 2013 Tom Vincent <http://tlvince.com/contact>
 
 vendor=".gitdata/vendor/gitignore/Global"
-cat $vendor/{Archives,SVN,Tags,OSX,vim}.gitignore > ".gitdata/gitignore"
+cat $vendor/{Archives,SVN,Tags,macOS,vim}.gitignore > ".gitdata/gitignore"
